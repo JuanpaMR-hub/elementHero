723d31a54bcd6e3b69281ccecc413d23035a7cb8
Juan Moreno <juanpablomoreno.r@gmail.com>
2023-11-28 01:48:13 -0300

    Agregada la funcion de verificar multiples elementos

--- main.js
+++ main.js
@@ -1,39 +1,91 @@
-alert("Bienvenido a Element Hero");
-alert("Para comenzar, abra la sección de inspección del navegador");
 
-console.log("Hola! \n Este es el inicio del juego")
-console.log("***INTRODUCCIÓN****");
-console.log("Los elementos que se mostrarán tienen uno que le combate, por ejemplo el fuego quema a la hierba, la hierba absorbe el agua, el agua apaga el fuego");
-console.log("En la siguiente tabla verás el 'counter' de cada uno");
-let elementos = ["Agua","Fuego","Hierba"];
-let counters = ["Hierba","Agua","Fuego"];
-counterTable = {elementos:elementos , counter:counters};
-
-console.table(counterTable);
+let elementos = ["Agua", "Fuego", "Hierba"];
+let counters = ["Hierba", "Agua", "Fuego"];
 
 
-function randomElement(){
+function showInfo(){
+    
+    let counterInfo ="0: Hierba\n1: Agua\n2: Fuego";
+
+    return counterInfo;
+
+}
+
+function randomElement() {
     let randomInt = Math.floor(Math.random() * 3);
     return elementos[randomInt];
 };
 
-function singleElementLevel(){
+function singleElementLevel() {
     let passed = false;
-    let randomPick = randomElement();
-    while (passed === false){
-        let answer = prompt(randomPick);
-        if(findIndex(answer) === findIndex(randomPick)){
+    do{
+        let randomPick = randomElement();
+        let answer = prompt(showInfo()+"\nQue elemento vence a:\n"+randomPick);
+        if (answer == elementos.indexOf(randomPick)) {
             passed = true;
             alert("Buen trabajo!");
             console.clear();
+        } else if(answer == null){
+            break;
         }else{
             alert("Intentalo denuevo");
         }
-    };
+
+    }while (passed === false);
+
+};
+
+function multipleElementsLevel(numElements) {
+    let passed = false;
+
+    do {
+        //Llenando array de elementos dependiendo de cuantos se necesiten
+        let elementsArray = [];
+        for (let i = 0; i < numElements; i++) {
+            elementsArray.push(randomElement());
+        };
+
+        //Respuesta del usuario "00"
+        let answer = prompt(showInfo()+"\nQue elementos vencen a:\n"+elementsArray.toString());
+
+        //Para poder compararlos, cambiaremos los elementos del array "elementsArray" y dejaremos sus respectivos index
+        for (let i = 0; i < elementsArray.length; i++) {
+            elementsArray[i] = elementos.indexOf(elementsArray[i]);
+        }
+
+        //Revisamos si la respuesta es correcta
+        if (answer.split("") == elementsArray.toString()) {
+            alert("Buen trabajo!");
+            passed = true;
+            console.clear();
+        } else {
+            alert("Respuesta incorrecta");
+        }
+
+    }while(passed === false);
+
+    
 
 }
 
-console.log("Se juega de la siguiente manera, escribe el index del counter del siguiente elemento");
+alert(`Bienvenido a Element Hero!`)
+alert(`***INTRODUCCIÓN****
+Los elementos que se mostrarán tienen uno que le combate, por ejemplo el fuego quema a la hierba, la hierba absorbe el agua, el agua apaga el fuego
+Se mostrará información de los elementos al igual que sus indices
+`)
+
+
+alert("Se juega de la siguiente manera, escribe el index del counter del siguiente elemento");
 singleElementLevel();
+
+
+
+alert("Felicidades! Has superado tu primer obstaculo \n intentemos otro \n Nuevamente aquí está la tabla de counters\n- Hierba vence a Agua\n- Agua vence a Fuego\n- Fuego vence a Agua");
+singleElementLevel();
+
+alert("Creo que ya lo tienes!");
+
+alert("oh.. ahora hay más elementos... ¿quizás si escribes los numeros continuos uno del otro funcionará? \n Ejemplo: '00'");
+multipleElementsLevel(4);
 
 
