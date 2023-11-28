5df1dc6c166735bb39117b308ab14108b6e48b6d
Juan Moreno <juanpablomoreno.r@gmail.com>
2023-11-28 19:58:03 -0300

    Creada nueva rama, mismo funcionamiento pero sin arrays

--- main.js
+++ main.js
@@ -1,30 +1,71 @@
 
-let elementos = ["Agua", "Fuego", "Hierba"];
-let counters = ["Hierba", "Agua", "Fuego"];
-
+let agua = "Agua";
+let fuego = "Fuego";
+let hierba = "Hierba";
 
 function showInfo(){
     
-    let counterInfo ="0: Hierba\n1: Agua\n2: Fuego";
-
+    let counterInfo ="1: Agua\n2: Fuego\n3: Hierba";
     return counterInfo;
 
 }
 
 function randomElement() {
     let randomInt = Math.floor(Math.random() * 3);
-    return elementos[randomInt];
+    console.log("Random Int"+randomInt);
+    switch(randomInt+1){
+        case 1:
+            console.log(agua);
+            return agua;
+        case 2:
+            console.log(fuego);
+            return fuego;
+        case 3:
+            console.log(hierba);
+            return hierba;
+        default:
+            console.log("Error al randomizar elemento");
+    }
 };
+
+function checkAnswer(answer,element){
+    console.log(answer + "==" + element) ;
+    switch(element){
+        
+        case "Agua":
+            //Hierba(3) vence a Agua            
+            if(answer == 3){
+                return true;
+            }
+            break;
+        case "Fuego":
+            //Agua(1) vence a Fuego
+            if(answer == 1){
+                return true;
+            }
+            break;
+        case "Hierba":
+            //Fuego(2) vence a Hierba
+            if(answer == 2 ){
+                return true;
+            }
+            break;
+        default:
+            console.log("Error al revisar respuesta");
+    }
+}
 
 function singleElementLevel() {
     let passed = false;
     do{
         let randomPick = randomElement();
         let answer = prompt(showInfo()+"\nQue elemento vence a:\n"+randomPick);
-        if (answer == elementos.indexOf(randomPick)) {
+        if (checkAnswer(answer,randomPick)) {
             passed = true;
             alert("Buen trabajo!");
             console.clear();
+        }else if(answer == "null"){
+            break;
         }else{
             alert("Intentalo denuevo");
         }
@@ -33,38 +74,6 @@ function singleElementLevel() {
 
 };
 
-function multipleElementsLevel(numElements) {
-    let passed = false;
-
-    do {
-        //Llenando array de elementos dependiendo de cuantos se necesiten
-        let elementsArray = [];
-        for (let i = 0; i < numElements; i++) {
-            elementsArray.push(randomElement());
-        };
-
-        //Respuesta del usuario "00"
-        let answer = prompt(showInfo()+"\nQue elementos vencen a:\n"+elementsArray.toString());
-
-        //Para poder compararlos, cambiaremos los elementos del array "elementsArray" y dejaremos sus respectivos index
-        for (let i = 0; i < elementsArray.length; i++) {
-            elementsArray[i] = elementos.indexOf(elementsArray[i]);
-        }
-
-        //Revisamos si la respuesta es correcta
-        if (answer.split("") == elementsArray.toString()) {
-            alert("Buen trabajo!");
-            passed = true;
-            console.clear();
-        } else {
-            alert("Respuesta incorrecta");
-        }
-
-    }while(passed === false);
-
-    
-
-}
 
 alert(`Bienvenido a Element Hero!`)
 alert(`***INTRODUCCIÓN****
@@ -78,21 +87,18 @@ singleElementLevel();
 
 
 
-alert("Felicidades! Has superado tu primer obstaculo \n intentemos otro \n Nuevamente aquí está la tabla de counters\n- Hierba vence a Agua\n- Agua vence a Fuego\n- Fuego vence a Agua");
+alert("Felicidades! Has superado tu primer obstaculo \n intentemos otro \n Nuevamente aquí está la tabla de counters\n- Agua vence a Fuego \n- Fuego vence a Agua\n- Hierba vence a Agua");
 singleElementLevel();
 
 alert("Creo que ya lo tienes!");
 
-alert("oh.. ahora hay más elementos... ¿quizás si escribes los numeros continuos uno del otro funcionará? \n Ejemplo: '00'");
-multipleElementsLevel(2);
+singleElementLevel();
 
-alert("Maravilloso!\n Ahora tienes lo necesario para continuar");
-multipleElementsLevel(4);
+alert("Muy bien!\nTienes lo necesario para convertirte en un Heroe de los Elementos")
 
 /*Posibles futuras funciones:
 1. Sistema de puntos
-2. Menu de acceso para seleccionar la cantidad de elementos
-3. Memorice donde no se disponga "show info"
+2. Memorice donde no se disponga "show info"
 */
 
 
